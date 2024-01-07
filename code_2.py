e7e1eda26c53bff6510d35208f175fe9ae3dfd1b
Uroma002 <146116449+uroma002@users.noreply.github.com>
2024-01-07 19:43:53 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,13 @@
+        for (int i = 0; i < 10; i++) {
+}
+            System.out.println("Random Number: " + item);
+        }
+        int[] data = new int[10];
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int item : data) {
+        }
+
+    public static void main(String[] args) {
+public class RandomDataGenerator {
+
+    }
